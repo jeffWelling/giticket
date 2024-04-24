7582f510b4e1dcb6c3b80685778e78366a61bd54
Jeff Welling <real.jeff.welling@gmail.com>
2024-04-24 00:37:14 -0700

    init works

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/jeffWelling/giticket
 
 go 1.21.6
+
+require github.com/jeffwelling/git2go/v37 v37.0.4
+
+require (
+	golang.org/x/crypto v0.0.0-20201203163018-be400aefbc4c // indirect
+	golang.org/x/sys v0.0.0-20201204225414-ed752295db88 // indirect
+)
--- internal/cli/actions/init.go
+++ internal/cli/actions/init.go
@@ -1,6 +1,12 @@
 package actions
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"time"
+
+	git "github.com/jeffwelling/git2go/v37"
+)
 
 // init() is used to register this action
 func init() {
@@ -13,8 +19,57 @@ type ActionInit struct {
 
 // Execute() is called when this action is invoked
 func (action *ActionInit) Execute() {
-	// TODO
 	fmt.Println("Initializing giticket")
+
+	// Open an existing repository in the current directory
+	repo, err := git.OpenRepository(".")
+	if err != nil {
+		log.Fatalf("Failed to open repo: %v", err)
+	}
+
+	// Create an index (staging area)
+	index, err := repo.Index()
+	if err != nil {
+		log.Fatalf("Failed to get repository index: %v", err)
+	}
+	defer index.Free()
+
+	// Write an empty tree from the index
+	treeID, err := index.WriteTree()
+	if err != nil {
+		log.Fatalf("Failed to write tree: %v", err)
+	}
+
+	// Get the tree object from its ID
+	tree, err := repo.LookupTree(treeID)
+	if err != nil {
+		log.Fatalf("Failed to get tree: %v", err)
+	}
+	defer tree.Free()
+
+	// Now create a commit with no parents (root commit)
+	author := &git.Signature{
+		Name:  "Your Name",
+		Email: "your.email@example.com",
+		When:  time.Now(),
+	}
+	oid, err := repo.CreateCommit("refs/heads/giticket", author, author, "Initial commit", tree)
+	if err != nil {
+		log.Fatalf("Failed to create commit: %v", err)
+	}
+
+	// Lookup the commit from its OID
+	commit, err := repo.LookupCommit(oid)
+	if err != nil {
+		log.Fatalf("Failed to find commit: %v", err)
+	}
+	defer commit.Free()
+
+	// Create an orphan branch called giticket
+	_, err = repo.CreateBranch("giticket", commit, true)
+	if err != nil {
+		log.Fatalf("Failed to create branch: %v", err)
+	}
 }
 
 // Help() prints help for this action
