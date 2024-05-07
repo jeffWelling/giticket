1671ceefd682c3c87b6d2800f7a6dc7872871897
Jeff Welling <real.jeff.welling@gmail.com>
2024-05-07 12:19:13 -0700

    improved list

--- internal/cli/subcommands/list.go
+++ internal/cli/subcommands/list.go
@@ -3,6 +3,7 @@ package subcommands
 import (
 	"flag"
 	"fmt"
+	"reflect"
 	"strings"
 
 	"github.com/jeffWelling/giticket/pkg/ticket"
@@ -54,14 +55,32 @@ func ListTickets(params listParams) string {
 	var ticketsList []ticket.Ticket
 	ticketsList = getListOfTickets(params.debug)
 
+	widthOfID := widest(ticketsList, "ID")
+	if widthOfID < 3 {
+		widthOfID = 3
+	}
+	widthOfTitle := widest(ticketsList, "Title")
+	if widthOfTitle < 20 {
+		widthOfTitle = 20
+	}
+	widthOfSeverity := widest(ticketsList, "Severity")
+	if widthOfSeverity < 9 {
+		widthOfSeverity = 9
+	}
+	widthOfStatus := widest(ticketsList, "Status")
+	if widthOfStatus < 10 {
+		widthOfStatus = 10
+	}
+
 	// Print the header
-	output += padRight("ID", 5) + "| " + padRight("Title", 30) + " | " + padRight("Severity", 10) + " | " + padRight("Status", 10) + "\n"
+	output += padRight("ID", widthOfID) + " | " + padRight("Title", widthOfTitle) + " | " + padRight("Severity", widthOfSeverity) + " | " + padRight("Status", widthOfStatus) + "\n"
+	output += strings.Repeat("-", widthOfID+widthOfTitle+widthOfSeverity+widthOfStatus+4) + "\n"
 
 	// Print the tickets
 	for _, t := range ticketsList {
 		IDAsString := fmt.Sprintf("%d", t.ID)
 		SeverityAsString := fmt.Sprintf("%d", t.Severity)
-		output += fmt.Sprintf("%s | %s | %s | %s\n", padRight(IDAsString, 4), padRight(t.Title, 30), padRight(SeverityAsString, 10), padRight(t.Status, 10))
+		output += fmt.Sprintf("%s | %s | %s | %s\n", padRight(IDAsString, widthOfID), padRight(t.Title, widthOfTitle), padRight(SeverityAsString, widthOfSeverity), padRight(t.Status, widthOfStatus))
 	}
 
 	return output
@@ -179,4 +198,32 @@ func padRight(s string, width int) string {
 		return s[0:width]
 	}
 	return s + strings.Repeat(" ", diff)
+}
+
+// widest() takes a list of tickets and a string representing the attribute name
+// of each ticket to check to find the widest string, and return that value
+func widest(tickets []ticket.Ticket, attr string) int {
+	widest := 0
+	for _, ticket := range tickets {
+		v := reflect.ValueOf(ticket)
+		if v.Kind() != reflect.Struct {
+			panic("not a struct")
+		}
+
+		fieldVal := v.FieldByName(attr)
+		if !fieldVal.IsValid() {
+			panic("not a valid field")
+		}
+		if !fieldVal.CanInterface() {
+			panic("cannot interface")
+		}
+
+		// if fieldVal is an int, conver it to string
+		fieldValString := fmt.Sprintf("%v", fieldVal.Interface())
+
+		if len(fieldValString) > widest {
+			widest = len(fieldValString)
+		}
+	}
+	return widest
 }
