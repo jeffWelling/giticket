f133ef2070bc08afbc8de86033d1a89ad68ead41
Jeff Welling <real.jeff.welling@gmail.com>
2024-06-14 02:29:15 -0700

    adding debug statements to delete

--- pkg/ticket/delete.go
+++ pkg/ticket/delete.go
@@ -28,30 +28,35 @@ func deleteTicket(ticketID int, branchName string, debugFlag bool) (bool, error)
 		return false, err
 	}
 
+	debug.DebugMessage(debugFlag, "Getting rootTreeBuilder and previousCommitTree from parent commit")
 	rootTreeBuilder, previousCommitTree, err := repo.TreeBuilderFromCommit(parentCommit, thisRepo, debugFlag)
 	if err != nil {
 		return false, err
 	}
 	defer rootTreeBuilder.Free()
 
+	debug.DebugMessage(debugFlag, "Getting .giticket subtree from previous commit")
 	giticketTree, err := repo.GetSubTreeByName(previousCommitTree, thisRepo, ".giticket", debugFlag)
 	if err != nil {
 		return false, err
 	}
 	defer giticketTree.Free()
 
+	debug.DebugMessage(debugFlag, "Getting tickets subtree from giticket tree")
 	giticketTreeBuilder, err := thisRepo.TreeBuilderFromTree(giticketTree)
 	if err != nil {
 		return false, err
 	}
 	defer giticketTreeBuilder.Free()
 
+	debug.DebugMessage(debugFlag, "Getting tickets subtree from giticket tree")
 	giticketTicketsTree, err := repo.GetSubTreeByName(giticketTree, thisRepo, "tickets", debugFlag)
 	if err != nil {
 		return false, err
 	}
 	defer giticketTicketsTree.Free()
 
+	debug.DebugMessage(debugFlag, "Getting tickets subtree builder from repo")
 	giticketTicketsTreeBuilder, err := thisRepo.TreeBuilderFromTree(giticketTicketsTree)
 	if err != nil {
 		return false, err
@@ -63,27 +68,41 @@ func deleteTicket(ticketID int, branchName string, debugFlag bool) (bool, error)
 	// Filter for ticketID
 	theTicket := FilterTicketsByID(ticketsList, ticketID)
 
-	giticketTicketsTreeBuilder.Remove(theTicket.TicketFilename())
+	// Remove ticket from tickets subtree
+	debug.DebugMessage(debugFlag, "Removing ticket "+strconv.Itoa(theTicket.ID)+" from tickets subtree")
+	err = giticketTicketsTreeBuilder.Remove(theTicket.TicketFilename())
+	if err != nil {
+		return false, err
+	}
 
+	debug.DebugMessage(debugFlag, "Writing giticket tickets subtree")
 	newGiticketTicketsTreeID, err := giticketTicketsTreeBuilder.Write()
 	if err != nil {
 		return false, err
 	}
 
-	giticketTreeBuilder.Insert("tickets", newGiticketTicketsTreeID, git.FilemodeTree)
+	debug.DebugMessage(debugFlag, "Adding tickets to giticket tree builder")
+	err = giticketTreeBuilder.Insert("tickets", newGiticketTicketsTreeID, git.FilemodeTree)
+	if err != nil {
+		return false, err
+	}
 
+	debug.DebugMessage(debugFlag, "Writing giticket tree")
 	newGiticketTreeID, err := giticketTreeBuilder.Write()
 	if err != nil {
 		return false, err
 	}
 
-	rootTreeBuilder.Insert(".giticket", newGiticketTreeID, git.FilemodeTree)
+	debug.DebugMessage(debugFlag, "Adding .giticket to root tree builder")
+	err = rootTreeBuilder.Insert(".giticket", newGiticketTreeID, git.FilemodeTree)
 
+	debug.DebugMessage(debugFlag, "Writing root tree")
 	newRootTreeID, err := rootTreeBuilder.Write()
 	if err != nil {
 		return false, err
 	}
 
+	debug.DebugMessage(debugFlag, "Looking up root tree")
 	newRootTree, err := thisRepo.LookupTree(newRootTreeID)
 	if err != nil {
 		return false, err
