d2f7c64f8f9f7b0bfd447b864b568f1931dff21f
Jeff Welling <real.jeff.welling@gmail.com>
2024-06-14 02:00:03 -0700

    add delete ticket

---
+++ internal/cli/subcommands/delete.go
@@ -0,0 +1,74 @@
+package subcommands
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/jeffwelling/giticket/pkg/common"
+	"github.com/jeffwelling/giticket/pkg/ticket"
+)
+
+func init() {
+	subcommand := new(SubcommandDelete)
+	registerSubcommand("delete", subcommand)
+}
+
+type SubcommandDelete struct {
+	flagset   *flag.FlagSet
+	params    map[string]interface{}
+	ticketID  int
+	debugFlag bool
+}
+
+func (subcommand *SubcommandDelete) InitFlags(args []string) error {
+	subcommand.params = make(map[string]interface{})
+	var (
+		helpFlag  bool
+		ticketID  int
+		debugFlag bool
+	)
+
+	subcommand.flagset = flag.NewFlagSet("delete", flag.ExitOnError)
+
+	subcommand.flagset.BoolVar(&debugFlag, "debug", false, "Print debug info")
+	subcommand.flagset.BoolVar(&helpFlag, "help", false, "Print help for the delete subcommand")
+	subcommand.flagset.IntVar(&ticketID, "ticketid", 0, "Ticket ID")
+	subcommand.flagset.IntVar(&ticketID, "id", 0, "Ticket ID")
+	subcommand.flagset.Parse(args)
+
+	// Sanity check
+	if ticketID == 0 {
+		fmt.Println("Error: ticketID is missing but is required to delete a ticket")
+		common.PrintGeneralUsage()
+		subcommand.Help()
+	}
+
+	subcommand.params["helpFlag"] = helpFlag
+	subcommand.params["ticketID"] = ticketID
+	subcommand.params["debugFlag"] = debugFlag
+	return nil
+}
+
+func (subcommand *SubcommandDelete) Execute() {
+	ticket.HandleDelete(subcommand.ticketID, common.BranchName, subcommand.debugFlag)
+}
+
+func (subcommand *SubcommandDelete) Help() {
+	fmt.Println("  delete - Delete ticket from the tree")
+	fmt.Println("    eg: giticket delete [parameters]")
+	fmt.Println("    parameters:")
+	fmt.Println("      --ticketid | --id N")
+	fmt.Println("      --debug")
+	fmt.Println("      --help")
+	fmt.Println("    examples:")
+	fmt.Println("      - name: Delete ticket with ID #1")
+	fmt.Println("        example: giticket delete --ticketid 1")
+}
+
+func (subcommand *SubcommandDelete) Parameters() map[string]interface{} {
+	return subcommand.params
+}
+
+func (subcommand *SubcommandDelete) DebugFlag() bool {
+	return subcommand.debugFlag
+}
---
+++ pkg/ticket/delete.go
@@ -0,0 +1,103 @@
+package ticket
+
+import (
+	"strconv"
+
+	git "github.com/jeffwelling/git2go/v37"
+	"github.com/jeffwelling/giticket/pkg/common"
+	"github.com/jeffwelling/giticket/pkg/debug"
+	"github.com/jeffwelling/giticket/pkg/repo"
+)
+
+// Delete the comment from the tree for the ticket with the given ID
+func HandleDelete(ticketID int, branchName string, debugFlag bool) {
+	debug.DebugMessage(debugFlag, "Deleting ticket "+strconv.Itoa(ticketID))
+
+}
+
+func deleteTicket(ticketID int, branchName string, debugFlag bool) (bool, error) {
+	debug.DebugMessage(debugFlag, "Opening git repository")
+	thisRepo, err := git.OpenRepository(".")
+	if err != nil {
+		return false, err
+	}
+
+	debug.DebugMessage(debugFlag, "Getting parent commit from branch '"+branchName+"'")
+	parentCommit, err := repo.GetParentCommit(thisRepo, branchName, debugFlag)
+	if err != nil {
+		return false, err
+	}
+
+	rootTreeBuilder, previousCommitTree, err := repo.TreeBuilderFromCommit(parentCommit, thisRepo, debugFlag)
+	if err != nil {
+		return false, err
+	}
+	defer rootTreeBuilder.Free()
+
+	giticketTree, err := repo.GetSubTreeByName(previousCommitTree, thisRepo, ".giticket", debugFlag)
+	if err != nil {
+		return false, err
+	}
+	defer giticketTree.Free()
+
+	giticketTreeBuilder, err := thisRepo.TreeBuilderFromTree(giticketTree)
+	if err != nil {
+		return false, err
+	}
+	defer giticketTreeBuilder.Free()
+
+	giticketTicketsTree, err := repo.GetSubTreeByName(giticketTree, thisRepo, "tickets", debugFlag)
+	if err != nil {
+		return false, err
+	}
+	defer giticketTicketsTree.Free()
+
+	giticketTicketsTreeBuilder, err := thisRepo.TreeBuilderFromTree(giticketTicketsTree)
+	if err != nil {
+		return false, err
+	}
+	defer giticketTicketsTreeBuilder.Free()
+
+	// Get ticket filename
+	ticketsList, err := GetListOfTickets(thisRepo, branchName, debugFlag)
+	// Filter for ticketID
+	theTicket := FilterTicketsByID(ticketsList, ticketID)
+
+	giticketTicketsTreeBuilder.Remove(theTicket.TicketFilename())
+
+	newGiticketTicketsTreeID, err := giticketTicketsTreeBuilder.Write()
+	if err != nil {
+		return false, err
+	}
+
+	giticketTreeBuilder.Insert("tickets", newGiticketTicketsTreeID, git.FilemodeTree)
+
+	newGiticketTreeID, err := giticketTreeBuilder.Write()
+	if err != nil {
+		return false, err
+	}
+
+	rootTreeBuilder.Insert(".giticket", newGiticketTreeID, git.FilemodeTree)
+
+	newRootTreeID, err := rootTreeBuilder.Write()
+	if err != nil {
+		return false, err
+	}
+
+	newRootTree, err := thisRepo.LookupTree(newRootTreeID)
+	if err != nil {
+		return false, err
+	}
+
+	// Get author data by reading .git configs
+	debug.DebugMessage(debugFlag, "getting author data")
+	author, err := common.GetAuthor(thisRepo)
+	if err != nil {
+		return false, err
+	}
+
+	// Commit
+	commitID, err := thisRepo.CreateCommit("refs/heads/giticket", author, author, "Deleting ticket "+theTicket.TicketFilename(), newRootTree, parentCommit)
+	debug.DebugMessage(debugFlag, "Commit ID "+commitID.String()+" created for deleting ticket "+theTicket.TicketFilename())
+	return true, err
+}
